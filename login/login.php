5f2fa05e7a443e966528bcd4b666fd4ac2a37552
Felipe Medina Retamal <felipe.medina1701@alumnos.ubiobio.cl>
2022-05-10 02:20:30 -0400

    commit test

--- login/login.php
+++ login/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar sesión</title>
+    <title>Iniciar una nueva sesión</title>
 </head>
 <body>
     <center>
