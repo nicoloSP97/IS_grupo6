97b7568d07433f688d791123baf046933136deef
Felipe Medina Retamal <felipe.medina1701@alumnos.ubiobio.cl>
2022-05-10 02:17:38 -0400

    comienzo

---
+++ login/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesión</title>
+</head>
+<body>
+    <center>
+        <h2>Ingresa tus datos a continuación:</h2>
+        <input type="text" placeholder="Usuario">
+        <input type="text" placeholder="Contraseña">
+        <button type="button">Ingresar</button>
+    </center>
+</body>
+</html>
