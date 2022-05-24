d20f5704c884fe2edd5283947a72ce122b9b1abc
NicolasArratia <nicolas.munoz1502@alumnos.ubiobio.cl>
2022-05-24 19:55:24 -0400

    register

---
+++ login/registrar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Login</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+  </head>
+  <body>
+    <section class="form-login">
+      <h5>Registrate</h5>
+      <h2 class="encabezado"> Bienvenido EDIFRED</h2>
+      <input class="controls" type="text" name="nombre" value="" placeholder="Nombre">
+      <input class="controls" type="numb" name="rut" value="" placeholder="Rut">
+      <input class="controls" type="numb" name="dpto" value="" placeholder="Numero de departamento">
+      <!--  <input class="controls" type="email" name="email" value="" placeholder="Email">  -->
+      <input class="controls" type="password" name="contrasena" value="" placeholder="Contraseña">
+      <input class="buttons" type="submit" name="" value="Registrar">
+      
+      <p><a href="#">¿Olvidastes tu Contraseña?</a></p>
+
+    </section>
+
+  </body>
+</html>
