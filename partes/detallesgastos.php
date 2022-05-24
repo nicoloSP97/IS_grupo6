7943d622adb455b85ab7516ab1bf8d8cbdd54f47
NicolasArratia <nicolas.munoz1502@alumnos.ubiobio.cl>
2022-05-23 20:18:08 -0400

    avance de meses pagados por el usuario

--- partes/detallesgastos.php
+++ partes/detallesgastos.php
@@ -37,3 +37,119 @@
                                         </div>
                     </div>
                 </section>
+                <section class="bg-mix py-3">
+                <div class="container">
+                    <div class="card rounded-0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-lg-3 col-md-6 d-flex stat my-3">
+                                    <div class="mx-auto">
+                                        <h6 class="text-muted">Luz</h6>
+                                        <h3 class="font-weight-bold">16.000</h3>
+                                        <h6 class="text-success"><i class="icon ion-md-arrow-dropup-circle"></i> 83.3%</h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-6 d-flex stat my-3">
+                                    <div class="mx-auto">
+                                        <h6 class="text-muted">Agua</h6>
+                                        <h3 class="font-weight-bold">25.000</h3>
+                                        <h6 class="text-success"><i class="icon ion-md-arrow-dropup-circle"></i> 50%</h6>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-6 d-flex stat my-3">
+                                    <div class="mx-auto">
+                                        <h6 class="text-muted">Gas</h6>
+                                        <h3 class="font-weight-bold">30.500</h3>
+                                        <h6 class="text-success"><i class="icon ion-md-arrow-dropup-circle"></i> 25%</h6>
+                                    </div>
+                                    
+                                </div>
+                                
+                                <div class="col-lg-3 col-md-6 d-flex my-3">
+                                    <div class="mx-auto">
+                                        <h6 class="text-muted">Otros</h6>
+                                        <h3 class="font-weight-bold">33.000</h3>
+                                        <h6 class="text-success"><i class="icon ion-md-arrow-dropup-circle"></i> 16.60%</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </section>
+
+              section>
+                  <div class="container">
+                      <div class="row">
+                          
+
+                          
+                          <div class="col-lg-4 my-3">
+                            <div class="card rounded-0">
+                                <div class="card-header bg-light">
+                                    <h6 class="font-weight-bold mb-0">Pago mes Mayo</h6>
+                                </div>
+                                <div class="card-body pt-2">
+                                <div class="d-flex border-bottom py-2">
+                                        <div class="d-flex mr-3">
+                                          <h2 class="align-self-center mb-0"><i class="far fa-bell"></i></h2>
+                                        </div>
+                                        <div class="align-self-center">
+                                        <h6 class="text-muted">luz</h6>
+                                            
+                                        <p class="lead text-muted">16.000</p>
+                                          
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="d-flex border-bottom py-2">
+                                        <div class="d-flex mr-3">
+                                          <h2 class="align-self-center mb-0"><i class="far fa-bell"></i></h2>
+                                        </div>
+                                        <div class="align-self-center">
+                                        <h6 class="text-muted">Agua</h6>
+                                            
+                                        <p class="lead text-muted">25.000</p>
+                                          
+                                        </div>
+                                    </div>
+
+                                    <div class="d-flex border-bottom py-2">
+                                        <div class="d-flex mr-3">
+                                          <h2 class="align-self-center mb-0"><i class="far fa-bell"></i></h2>
+                                        </div>
+                                        <div class="align-self-center">
+                                        <h6 class="text-muted">Gas</h6>
+                                            
+                                        <p class="lead text-muted">30.500</p>
+                                          
+                                        </div>
+                                    </div>
+
+                                    <div class="d-flex border-bottom py-2">
+                                        <div class="d-flex mr-3">
+                                          <h2 class="align-self-center mb-0"><i class="far fa-bell"></i></h2>
+                                        </div>
+                                        <div class="align-self-center">
+                                        <h6 class="text-muted">Otros</h6>
+                                            
+                                        <p class="lead text-muted">33.000</p>
+                                          
+                                        </div>
+                                    </div>
+                                    <div class="d-flex border-bottom py-2">
+                                        <div class="d-flex mr-3">
+                                          <h2 class="align-self-center mb-0"><i class="far fa-bell"></i></h2>
+                                        </div>
+                                        <div class="align-self-center">
+                                        <h6 class="text-muted">Total</h6>
+                                        <h3 class="font-weight-bold">104.000</h3>
+                                        <span class="badge badge-success ml-2" >total a pagar</span>
+                                          <br>
+                                        </div>
+                                    </div>
+                                    
+                                    <br>
+                                    <button class="btn btn-primary w-100">Pagar</button>
+                                </div>
+                            </div>
