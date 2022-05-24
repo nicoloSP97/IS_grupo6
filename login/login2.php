a67e452c66f0e556236c3c727288367edef9c89d
NicolasArratia <nicolas.munoz1502@alumnos.ubiobio.cl>
2022-05-24 13:23:57 -0400

    creacion de version de login v2

---
+++ login/login2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Login</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+  </head>
+  <body>
+    <section class="form-login">
+      <h5>Login</h5>
+      <h2 class="encabezado"> Bienvenido a EDIFRED</h2>
+      <input class="controls" type="text" name="nombre" value="" placeholder="Nombre">
+      <input class="controls" type="numb" name="rut" value="" placeholder="Rut">
+      <input class="controls" type="password" name="contrasena" value="" placeholder="Contraseña">
+      <input class="buttons" type="submit" name="" value="Ingresar">
+      <p><a href="#">¿Olvidastes tu Contraseña?</a></p>
+
+    </section>
+
+  </body>
+</html>
