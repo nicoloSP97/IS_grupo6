f1743d23cfa857fc78edc2eb813d57ead03036b3
NicolasArratia <nicolas.munoz1502@alumnos.ubiobio.cl>
2022-05-23 20:16:06 -0400

    avance de detalles de pago usuario

---
+++ partes/detallesgastos.php
@@ -0,0 +1,39 @@
+ <!-- head -->
+ <?php include('../partes/head.php') ?>
+    <!-- fin head -->
+
+
+<body>
+    <div class="d-flex" id="content-wrapper">
+    <!-- sideBar -->
+    <?php include('../partes/sidebar.php') ?>
+    <!-- fin sideBar -->
+
+        <div class="w-100">
+
+    <!-- Navbar -->
+        <?php include('../partes/nav.php') ?>
+    <!-- Fin Navbar -->
+
+        <!-- Page Content -->
+        <div id="content" class="bg-grey w-100">
+
+                <section class="bg-light py-3">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-9 col-md-8">
+                                <h1 class="font-weight-bold mb-0">Bienvenido Juan</h1>
+                                <h2 class="font-weight-bold mb-0">Dpto N° 201</h2>
+                                
+                                <p class="lead text-muted">Revisa la última información</p>
+                            </div>
+                            <div class="col-lg-3 col-md-4 d-flex">
+                                <button class="btn btn-primary w-100 align-self-center">Fechas por vencer</button>
+                            </div>
+                        </div>
+                        <div class="align-self-center">
+                                          <h6 class="d-inline-block mb-0">Estado de deudas</h6><span class="badge badge-warning ml-2">por pagar</span>
+                                          
+                                        </div>
+                    </div>
+                </section>
