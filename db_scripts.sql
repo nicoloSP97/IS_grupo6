93fa8f19c0ddf8f6713473148d01d4ae28712bb6
nicoloSP97 <nicolas.silva1801@alumnos.ubiobio.cl>
2022-06-16 23:24:13 -0400

    scripts

--- db_scripts.sql
+++ db_scripts.sql
@@ -1,9 +1,3 @@
-Creacion de 4 departamento que son :101,102,201,202 estos son parte del edificio 01
-    insert into departamento values (101,'desocupado'),(102,'desocupado'),(201,'desocupado'),(202,'desocupado');
-
-definiendo roles en tabla tipo_usuario
-    insert INTO tipo_usuario(cod_usuario,rol_usuario) values(11,'administrador');
-    insert INTO tipo_usuario(cod_usuario,rol_usuario) values(22,'arrendatario');
 
 creacion tabla tipo_usuario:
 CREATE TABLE tipo_usuario( cod_usuario int, rol_usuario varchar(20), primary key (cod_usuario) );
@@ -28,4 +22,10 @@ id_depa int, rut varchar(20),
 FOREIGN KEY (id_depa) REFERENCES departamento(id_depa), 
 FOREIGN Key (rut) REFERENCES usuario(rut) ); 
 
-creacion
+creacion..
+Creacion de 4 departamento que son :101,102,201,202 estos son parte del edificio 01
+    insert into departamento values (101,'desocupado'),(102,'desocupado'),(201,'desocupado'),(202,'desocupado');
+
+definiendo roles en tabla tipo_usuario
+    insert INTO tipo_usuario(cod_usuario,rol_usuario) values(11,'administrador');
+    insert INTO tipo_usuario(cod_usuario,rol_usuario) values(22,'arrendatario');
